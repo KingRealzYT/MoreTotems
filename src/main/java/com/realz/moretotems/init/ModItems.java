554d54ba1a8fbdf241c8a7a4282e05877fec61fd
nicklasdev <51082185+nicklascode@users.noreply.github.com>
2022-06-08 20:33:45 +0200

    Stuff

--- src/main/java/com/realz/moretotems/init/ModItems.java
+++ src/main/java/com/realz/moretotems/init/ModItems.java
@@ -3,6 +3,7 @@ package com.realz.moretotems.init;
 import com.realz.moretotems.MoreTotems;
 import com.realz.moretotems.essence.EssenceBase;
 import com.realz.moretotems.totems.TotemBase;
+import com.realz.moretotems.totems.chickenTotem;
 import net.minecraft.world.item.Item;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
@@ -16,6 +17,6 @@ public class ModItems {
     public static final RegistryObject<Item> CHICKEN_ESSENCE = ITEMS.register("chicken_essence", EssenceBase::new);
 
     // Totems
-    public static final RegistryObject<Item> CHICKEN_TOTEM = ITEMS.register("chicken_totem", TotemBase::new);
+    public static final RegistryObject<Item> CHICKEN_TOTEM = ITEMS.register("chicken_totem", chickenTotem::new);
 
 }
--- src/main/java/com/realz/moretotems/totems/TotemBase.java
+++ src/main/java/com/realz/moretotems/totems/TotemBase.java
@@ -1,8 +1,16 @@
 package com.realz.moretotems.totems;
 
 import com.realz.moretotems.MoreTotems;
+import com.realz.moretotems.init.ModItems;
+import net.minecraft.advancements.CriteriaTriggers;
+import net.minecraft.advancements.CriterionTrigger;
+import net.minecraft.advancements.critereon.UsedTotemTrigger;
+import net.minecraft.world.InteractionResult;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.Rarity;
+import net.minecraft.world.item.context.UseOnContext;
+import net.minecraftforge.event.RegistryEvent;
 
 public class TotemBase extends Item {
 
@@ -10,4 +18,19 @@ public class TotemBase extends Item {
         super(new Item.Properties().tab(MoreTotems.TOTEMTAB).stacksTo(1).rarity(Rarity.UNCOMMON));
     }
 
+
+    @Override
+    public InteractionResult useOn(UseOnContext pContext){
+        Player p = pContext.getPlayer();
+        if(pContext.getLevel().isClientSide()){
+            System.out.println(pContext.getItemInHand());
+
+        }
+        pContext.getItemInHand().hurtAndBreak(16, p,(player) -> player.broadcastBreakEvent(player.getUsedItemHand()));
+        return super.useOn(pContext);
+    }
 }
+
+
+
+
