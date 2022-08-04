f2162265d5d68328531b90f0a4175aa0a357c890
Yusuf Arfan Ismail <yusufgamer222@gmail.com>
2022-08-04 22:15:20 +0100

    add en_lang data gen

--- src/main/java/com/realz/moretotems/data/lang/ModEnLangProvider.java
+++ src/main/java/com/realz/moretotems/data/lang/ModEnLangProvider.java
@@ -1,6 +1,7 @@
 package com.realz.moretotems.data.lang;
 
 import com.realz.moretotems.MoreTotems;
+import com.realz.moretotems.init.ModItems;
 import net.minecraft.data.DataGenerator;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
@@ -14,7 +15,10 @@ public class ModEnLangProvider extends LanguageProvider {
 
     @Override
     protected void addTranslations() {
-
+        add(ModItems.CHICKEN_ESSENCE.get(), "Chicken Essence");
+        add(ModItems.CHICKEN_TOTEM.get(), "Chicken Totem");
+        add(MoreTotems.TOTEMTAB.getDisplayName().getString(), "TotemMod | Totem Tab");
+        add(MoreTotems.ESSENCETAB.getDisplayName().getString(), "TotemMod | Essence Tab");
     }
 
     protected <T extends Item> void item(RegistryObject<T> entry, String name) {
